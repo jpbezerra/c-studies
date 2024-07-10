4eb8432ef4b9bb02c2caa89690fc78088ceec158
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-07-09 22:53:55 -0300

    Update quiz.c

--- C-studies/quiz.c
+++ C-studies/quiz.c
@@ -1,97 +1,97 @@
-   #include <stdio.h>
-   #include <ctype.h>
-   #include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-   enum Difficulty{
-      easy = 1,
-      medium = 2
+enum Difficulty{
+   easy = 1,
+   medium = 2
+};
+
+int main() {
+   printf("QUIZ GAME\n"); 
+
+   char questions[][100] = {"1. What year did the C language debut?: ",
+                           "2. Who is credited with creating C?: ",
+                           "3. What is the predecessor of C?: ",
+                           "4. How many bits has 8 bytes?: ",
+                           "5. What is the symbol for the element gold?: ",
+                           "6. What is the name of the world's longest river?: ",
+                           "7. What is the capital of Liechtenstein: ?"
    };
-
-   int main() {
-      printf("QUIZ GAME\n"); 
-
-      char questions[][100] = {"1. What year did the C language debut?: ",
-                              "2. Who is credited with creating C?: ",
-                              "3. What is the predecessor of C?: ",
-                              "4. How many bits has 8 bytes?: ",
-                              "5. What is the symbol for the element gold?: ",
-                              "6. What is the name of the world's longest river?: ",
-                              "7. What is the capital of Liechtenstein: ?"
-      };
                               
-      char options[][100] = {"A. 1969", "B. 1972", "C. 1975", "D. 1999",
-                           "A. Dennis Ritchie", "B. Nikola Tesla", "C. John Carmack", "D. Doc Brown",
-                           "A. Objective C", "B. B", "C. C++", "D. C#",
-                           "A. 16", "B. 64", "C. 4", "D. 32",
-                           "A. Fe", "B. Cu", "C. Au", "D. Pt",
-                           "A. Nile", "B. Amazon", "C. Yangtze", "D. Mississippi",
-                           "A. Vaduz", "B. Berna", "C. Liubliana", "D. Bratislava",
-      };
+   char options[][100] = {"A. 1969", "B. 1972", "C. 1975", "D. 1999",
+                        "A. Dennis Ritchie", "B. Nikola Tesla", "C. John Carmack", "D. Doc Brown",
+                        "A. Objective C", "B. B", "C. C++", "D. C#",
+                        "A. 16", "B. 64", "C. 4", "D. 32",
+                        "A. Fe", "B. Cu", "C. Au", "D. Pt",
+                        "A. Nile", "B. Amazon", "C. Yangtze", "D. Mississippi",
+                        "A. Vaduz", "B. Berna", "C. Liubliana", "D. Bratislava",
+   };
                            
-      char answers[] = {'B', 'A', 'B', 'D', 'C', 'A'};
-      int quest_size = sizeof(questions)/sizeof(questions[0]);
+   char answers[] = {'B', 'A', 'B', 'D', 'C', 'A'};
+   int quest_size = sizeof(questions)/sizeof(questions[0]);
 
-      enum Difficulty diff;
+   enum Difficulty diff;
 
-      printf("Choose your difficulty Easy (1) or Medium (2)? ");
-      scanf("%d", &diff);
+   printf("Choose your difficulty Easy (1) or Medium (2)? ");
+   scanf("%d", &diff);
 
-      diff == easy ? printf("Easy mode chosen!\n") : printf("Medium mode chosen!\n(this mode is kinda hard ngl");
+   diff == easy ? printf("Easy mode chosen!\n") : printf("Medium mode chosen!\n(this mode is kinda hard ngl");
 
-      bool wrong_medium = false;
-      int quest_miss;
+   bool wrong_medium = false;
+   int quest_miss;
 
-      char guess;
-      int score = 0;   
+   char guess;
+   int score = 0;   
 
-      for(int i = 0; i < quest_size; i++) {
-         printf("\n*********************\n");
-         printf("%s\n", questions[i]);
-         printf("*********************\n");
+   for(int i = 0; i < quest_size; i++) {
+      printf("\n*********************\n");
+      printf("%s\n", questions[i]);
+      printf("*********************\n");
 
-         for(int j = (i * 4); j < (i * 4) + 4; j++) {
-            printf("%s\n", options[j]);
+      for(int j = (i * 4); j < (i * 4) + 4; j++) {
+         printf("%s\n", options[j]);
+      }
+
+      printf("guess: ");
+      scanf("%c ", &guess);
+
+      guess = toupper(guess);
+
+      if(guess == answers[i]) {
+         printf("CORRECT!\n");
+         score++;
+      }
+
+      else {
+         printf("WRONG!\n");
+         if (diff == medium) {
+            wrong_medium = true;   
          }
+      }
 
-         printf("guess: ");
-         scanf("%c ", &guess);
-
-         guess = toupper(guess);
-
-         if(guess == answers[i]) {
-            printf("CORRECT!\n");
-            score++;
-         }
-
-         else {
-            printf("WRONG!\n");
-            if (diff == medium) {
-               wrong_medium = true;   
-            }
-         }
-
-         if (wrong_medium) {
-            quest_miss = i + 1;
-            break;
-         }
+      if (wrong_medium) {
+         quest_miss = i + 1;
+         break;
+      }
          
-      }
-
-      if (!wrong_medium) {
-         printf("\n*********************\n");
-         printf("FINAL SCORE: %d/%d\n", score, quest_size);
-         printf("*********************\n");
-   
-         float rate = (float) score * 100 / quest_size;
-         printf("You had a rate of %.2f!", rate);
-      }
-      
-      else if (wrong_medium) {
-         printf("\n*********************\n");
-         printf("Unfortunately you got the question number %d wrong!\n", quest_miss);
-         printf("I told you that this mode is kinda hard!");
-         printf("*********************\n");
-      }
-
-      return 0;
    }
+
+   if (!wrong_medium) {
+      printf("\n*********************\n");
+      printf("FINAL SCORE: %d/%d\n", score, quest_size);
+      printf("*********************\n");
+   
+      float rate = (float) score * 100 / quest_size;
+      printf("You had a rate of %.2f!", rate);
+   }
+      
+   else if (wrong_medium) {
+      printf("\n*********************\n");
+      printf("Unfortunately you got the question number %d wrong!\n", quest_miss);
+      printf("I told you that this mode is kinda hard!");
+      printf("*********************\n");
+   }
+
+   return 0;
+}
